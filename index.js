0276437d8eacf11b480b6baba01c6a01a61beaf7
Tanmay Gupta <43621750+tanmayGypt@users.noreply.github.com>
2023-09-30 17:20:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express= require("express");
+const bodyparser=require("body-parser");
+let ejs = require('ejs');
+const app= express();
+app.use(bodyparser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(express.static("static"));
+
+
+app.get("/", (req,res)=>{
+    res.render("homepage")
+})
+
+
+app.get("/contact", (req,res)=>{
+    res.render("contact card")
+})
+
+app.get("/login", (req,res)=>{
+    res.render("login")
+})
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Server started");
+})
